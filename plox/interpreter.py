e0f657544a4ac1c99677de732833d95968c15ab5
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2025-01-28 00:19:10 +0000

    build up interpreter

--- plox/interpreter.py
+++ plox/interpreter.py
@@ -1,13 +1,62 @@
-from typing import override
-from plox.expr import Expr, ExprVisitor, Grouping, Literal
+from typing import Any, override
+from plox.expr import Binary, Expr, ExprVisitor, Grouping, Literal, Unary
+from plox.token import Token
+from plox.token_type import TokenType
 
 
 class Interpreter(ExprVisitor):
-    @override
-    def visit_binary_expr(self, expr: "Expr") -> None:
-        pass
+    def check_if_number(self, operator: Token, left: Any, right: Any) -> None:
+        if isinstance(left, int | float) and isinstance(right, int | float):
+            return
 
-    def evaluate(self, expr: Expr) -> object:
+        raise RuntimeError(operator, "Operands must be numeric objects.")
+
+    @staticmethod
+    def is_equal(a: Any, b: Any) -> bool:
+        return a == b
+
+    @override
+    def visit_binary_expr(self, expr: Binary) -> object:
+        left: Any = self.evaluate(expr.left)
+        right: Any = self.evaluate(expr.right)
+
+        if expr.operator.type == TokenType.PLUS:
+            if (isinstance(left, int | float) and isinstance(right, int | float)) or (
+                isinstance(left, str) and isinstance(right, str)
+            ):
+                return left + right
+            raise RuntimeError(expr.operator, "Operands must be two strings or two numeric objects.")
+        elif expr.operator.type == TokenType.MINUS:
+            self.check_if_number(expr.operator, left, right)
+            return left - right
+        elif expr.operator.type == TokenType.SLASH:
+            self.check_if_number(expr.operator, left, right)
+            return left / right
+        elif expr.operator.type == TokenType.STAR:
+            self.check_if_number(expr.operator, left, right)
+            return left * right
+        elif expr.operator.type == TokenType.GREATER:
+            self.check_if_number(expr.operator, left, right)
+            return left > right
+        elif expr.operator.type == TokenType.GREATER_EQUAL:
+            self.check_if_number(expr.operator, left, right)
+            return left >= right
+        elif expr.operator.type == TokenType.LESS:
+            self.check_if_number(expr.operator, left, right)
+            return left < right
+        elif expr.operator.type == TokenType.LESS_EQUAL:
+            self.check_if_number(expr.operator, left, right)
+            return left <= right
+        elif expr.operator.type == TokenType.BANG_EQUAL:
+            self.check_if_number(expr.operator, left, right)
+            return not self.is_equal(left, right)
+        elif expr.operator.type == TokenType.EQUAL_EQUAL:
+            self.check_if_number(expr.operator, left, right)
+            return self.is_equal(left, right)
+
+        return None
+
+    def evaluate(self, expr: Expr) -> Any:
         return expr.accept(self)
 
     @override
@@ -18,9 +67,18 @@ class Interpreter(ExprVisitor):
     def visit_literal_expr(self, expr: Literal) -> object:
         return expr.value
 
+    @staticmethod
+    def is_truthy(obj: Any) -> bool:
+        return bool(obj)
+
     @override
-    def visit_unary_expr(self, expr: "Expr") -> None:
-        pass
+    def visit_unary_expr(self, expr: Unary) -> object:
+        right = self.evaluate(expr.right)
+        if expr.operator.type is TokenType.BANG:
+            return not self.is_truthy(right)
+        if expr.operator.type is TokenType.MINUS:
+            return -right
+        return None
 
     @override
     def visit_variable_expr(self, expr: "Expr") -> None:
