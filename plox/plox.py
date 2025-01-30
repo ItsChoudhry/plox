0c5b593fe627925874e9745171e23ceb0ca263fd
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2025-01-30 01:01:32 +0000

    Wire up interpreter (we got a calculator)

--- plox/plox.py
+++ plox/plox.py
@@ -1,4 +1,5 @@
-from plox.interpreter import LoxRuntimeError
+import sys
+from plox.interpreter import Interpreter, LoxRuntimeError
 from plox.token import Token
 from plox.token_type import TokenType
 from plox.scanner import Scanner
@@ -17,11 +18,15 @@ logger = logging.getLogger(__name__)
 
 
 class Plox:
+    interpreter = Interpreter()
+    had_error = False
     had_runtime_error = False
 
     @staticmethod
     def report(line: int, where: str, message: str):
         logger.info("[%d] Error: %s: %s", line, where, message)
+
+        Plox.had_error = True
 
     @staticmethod
     def error(token: Token, message: str) -> None:
@@ -44,20 +49,44 @@ class Plox:
         parser: Parser = Parser(tokens)
         expression: Expr = parser.parse()
 
-        print("exprs")
-        print(expression)
-        print("printer")
+        # print("exprs")
+        # print(expression)
+        # print("printer")
         print(AstPrinter().print(expression))
+        Plox.interpreter.interpret(expression)
 
     def runFile(self, path: str):
         with open(path) as f:
             file = f.read(-1)
             self.run(file)
 
+            if Plox.had_error:
+                sys.exit(65)
+            elif Plox.had_runtime_error:
+                sys.exit(70)
+
+    @staticmethod
+    def repl_intro() -> None:
+        python_version = ".".join(str(i) for i in sys.version_info[:3])
+        print(
+            "Plox REPL",
+            f"[Python {python_version}] on {sys.platform}",
+            'Type "copyright", "credits" or "license" for more information.',
+            'Type "exit" or press Ctrl-D (i.e. EOF) to leave.',
+            sep="\n",
+        )
+
     @staticmethod
     def runPrompt():
-        while 1:
-            line = input("> ")
-            if line == "exit" or line == "":
-                break
-            Plox.run(line)
+        Plox.repl_intro()
+        while True:
+            try:
+                line = input("> ")
+                if line == "exit" or line == "":
+                    break
+                Plox.run(line)
+
+            except KeyboardInterrupt as k:
+                print(f"\n{k.__class__.__name__}")
+            except EOFError:
+                sys.exit(0)
